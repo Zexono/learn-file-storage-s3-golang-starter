08b17eb3bae93abb1c44ef7ad3c115982ae4d655
Zexono <spector6622@gmail.com>
2026-01-13 03:44:59 +0700

    AWS + S3 video upload

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,110 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	const maxMemory = 1 << 30
+	http.MaxBytesReader(w,r.Body,maxMemory)
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	viedeo_db ,err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "No video id here", err)
+		return
+	}
+
+	if viedeo_db.UserID != userID {
+    respondWithError(w, http.StatusUnauthorized, "Not video owner", nil)
+    return
+	}
+
+	//r.ParseMultipartForm(maxMemory)
+
+	multiPartfile, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer multiPartfile.Close()
+
+	contentTypes := header.Header.Get("Content-Type")
+
+	media_type ,_,err := mime.ParseMediaType(contentTypes)
+	if  media_type != "video/mp4"{
+		respondWithError(w, http.StatusInternalServerError, "header contentType not video", err)
+		return
+	}
+
+	tmp ,err := os.CreateTemp("","tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to create tmp", err)
+		return
+	}
+	defer os.Remove(tmp.Name())
+	defer tmp.Close()
+
+	if _, err = io.Copy(tmp, multiPartfile); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error saving file", err)
+		return
+	}
+
+	tmp.Seek(0,io.SeekStart)
+
+	key := make([]byte, 32)
+	rand.Read(key)
+	filename := base64.RawURLEncoding.EncodeToString(key)
+	assetPath := getAssetPath(filename, contentTypes)
+
+	cfg.s3Client.PutObject(context.Background(),&s3.PutObjectInput{
+		Bucket: &cfg.s3Bucket,
+		Key: &assetPath,
+		Body: tmp,
+		ContentType: &media_type,
+	})
+
+	url := cfg.getVideoAssetURL(cfg.s3Bucket, cfg.s3Region, assetPath)
+	viedeo_db.VideoURL = &url
+
+	err = cfg.db.UpdateVideo(viedeo_db)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "can't update vod data", err)
+		return
+	}
+
+	
+	respondWithJSON(w, http.StatusOK, viedeo_db)
+
+	
+
+
+}
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 
@@ -18,6 +21,7 @@ type apiConfig struct {
 	platform         string
 	filepathRoot     string
 	assetsRoot       string
+	s3Client		 *s3.Client
 	s3Bucket         string
 	s3Region         string
 	s3CfDistribution string
@@ -84,12 +88,19 @@ func main() {
 		log.Fatal("PORT environment variable is not set")
 	}
 
+	aws_config ,err:= config.LoadDefaultConfig(context.Background(),config.WithRegion(s3Region))
+	if err != nil {
+		log.Fatalf("Couldn't Load AWS config: %v", err)
+	}
+	s3Client := s3.NewFromConfig(aws_config)
+
 	cfg := apiConfig{
 		db:               db,
 		jwtSecret:        jwtSecret,
 		platform:         platform,
 		filepathRoot:     filepathRoot,
 		assetsRoot:       assetsRoot,
+		s3Client: 		  s3Client,
 		s3Bucket:         s3Bucket,
 		s3Region:         s3Region,
 		s3CfDistribution: s3CfDistribution,
